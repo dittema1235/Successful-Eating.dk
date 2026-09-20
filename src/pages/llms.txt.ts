44a5c91e249d69c241eaf9d8fcf05b6bb3b60333
dittema1235 <hello@successfuleating.dk>
2026-09-20 10:40:03 +0200

    Ret cand.psych.aut. i sidebilledtekster
    Ditte er cand.psych.aut. (autoriseret psykolog). 2004 er året, hvor hun blev færdig med sin kandidatuddannelse (cand.psych.) fra Københavns Universitet. Opdaterer figurtekst på behandlingssiderne, sidebar og faglige baggrund på Om Ditte, forsidens checkliste, footer og llms.txt, så autorisationen fremgår korrekt.

--- src/pages/llms.txt.ts
+++ src/pages/llms.txt.ts
@@ -5,13 +5,13 @@ export function GET() {
   return new Response(
     `# Successful Eating
 
-> Dansksproget viden om overspisning og madro samt psykologiske forløb hos klinisk psykolog Ditte Munch-Andersen, cand.psych. fra Københavns Universitet (2004). Online i Danmark og klinik på Hovedvagtsstræde 2C, 3000 Helsingør. CVR 30642155.
+> Dansksproget viden om overspisning og madro samt psykologiske forløb hos klinisk psykolog Ditte Munch-Andersen, cand.psych.aut., uddannet fra Københavns Universitet (2004). Online i Danmark og klinik på Hovedvagtsstræde 2C, 3000 Helsingør. CVR 30642155.
 
 ## Tilbud
 - [Behandling af overspisning](https://www.successfuleating.dk/forloebet): Successful Eating. ${formatPrice(site.treatmentPrice)} inklusive en gratis individuel forsamtale. Vurderes forløbet ved forsamtalen at være et dårligt match, tilbagebetales hele det indbetalte beløb. Alternativt kan en forsamtale på ${site.consultationMinutes} minutter købes for ${formatPrice(site.consultationPrice)}; beløbet modregnes, hvis man efterfølgende vælger forløbet (restbeløb ${formatPrice(site.treatmentPrice - site.consultationPrice)}).
 - [Kropsaccept](https://www.successfuleating.dk/kropsglaede): Individuel psykologisk behandling med fokus på kropskritik.
 - ADHD er et videnstema og ikke et separat forløb.
-- [Kontakt](https://www.successfuleating.dk/kontakt): ${site.email}. Telefon +45 71 41 59 69.
+- [Kontakt](https://www.successfuleating.dk/kontakt): hello@successfuleating.com. Telefon +45 71 41 59 69.
 - [Om Ditte Munch-Andersen](https://www.successfuleating.dk/om-ditte): Faglig baggrund og behandlingsmetode.
 
 ## Viden
